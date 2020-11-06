f234181b19446acae86312b5a5996bcf5a550bd0
Brigitta Szikszai <sziszka0001@gmail.com>
2020-11-06 15:15:56 +0000

    add yab function and more media query for mobile

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-//$( "button.anatomy" ).html( "" )
+var tabBtns = document.querySelectorAll('.tab-btn');
+var tabContent = document.querySelectorAll('.tab-content');
 
-var hiddenBox = $( ".anatomy-content" );
-$( ".anatomy button" ).on( "click", function( event ) {
-  hiddenBox.show();
-});
+//binding event
+for(let i=0;i<tabBtns.length;i++){
+  tabBtns[i].addEventListener('click', function(ev) {
+    openTab(ev, tabBtns[i])
+  });
+}
+
+function openTab(ev, btn){
+  //removing active id
+  for(let i=0;i<tabBtns.length;i++){
+    tabBtns[i].setAttribute('id',"");
+    tabContent[i].setAttribute('id',"");
+
+    // reset all images back to white
+    let img = tabBtns[i].querySelector('img');
+    img.setAttribute('src', img.getAttribute('src').replace('red', 'white'));
+  }
+
+  //displaying target
+  btn.setAttribute('id',"active-tab");
+  let t = btn.dataset.tab;
+  console.log(btn);
+  document.querySelectorAll('.tab-content')[t].setAttribute('id','active-content');
+
+  // find the image in button, set to red
+  let img = tabBtns[i].querySelector('img');
+  img.setAttribute('src', img.getAttribute('src').replace('white', 'red'));
+}
+
